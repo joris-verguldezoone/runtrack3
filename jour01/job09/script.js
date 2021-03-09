583dd9395dc270f5854f60cf530beab97b4c9f68
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-03-09 10:13:29 +0100

    en mieux

--- jour01/job09/script.js
+++ jour01/job09/script.js
@@ -12,18 +12,48 @@ function desc(x, y) { // je voulais pas déclarer d'autre fonction mais j'ai tro
 function asc(x, y) {
     return x - y;
 }
+
 // fonction de tri 
 
 function tri(numbers, order) {
-    if (order == 'asc') {
-        result = numbers.sort(asc);
-        console.log(result);
+
+    let newArray = numbers;
+    let temp = numbers;
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = 0; j < numbers.length; j++) {
+            switch (order) {
+                case 'desc':
+                    if (numbers[i] > newArray[j]) {
+                        temp = numbers[i];
+                        numbers[i] = newArray[j];
+                        newArray[j] = temp;
+                    }
+                    break;
+                case 'asc':
+                    if (numbers[i] < newArray[j]) {
+                        temp = numbers[i];
+                        numbers[i] = newArray[j]
+                        newArray[j] = temp;
+                    }
+                    break;
+            }
+
+
+        }
     }
-    if (order == "desc") {
-        result = numbers.sort(desc);
-        console.log(result);
-    }
+    return console.log(newArray);
+
+
+    //     if (order == 'asc') {
+    //         result = numbers.sort(asc);
+    //         console.log(result);
+    //     }
+    //     if (order == "desc") {
+    //         result = numbers.sort(desc);
+    //         console.log(result);
+    //     }
 }
 
-numbers = [1, 2, 3, 4, 5, 6, 7]; // on donne un tableau prédéfini 
+numbers = [5, 8, 10, 13, 2, 3, 4, 6, 7]; // on donne un tableau prédéfini 
 tri(numbers, 'asc');
