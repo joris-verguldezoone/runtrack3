f92b531d40eee1a9e7f1120dcd4a2493b43f3c9b
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-03-16 11:25:17 +0100

    Bootstrap c pour les indiens et les chinois

---
+++ jour07/job01/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <title>job01</title>
+    <link rel='stylesheet' href='style.css'>
+</head>
+<body style="background-color:lightgrey;">
+    <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">LPTF</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Units</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Jobs</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Skills</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </header>
+
+    <main>
+    <h1  class="fw-lighter text-center" >LaPlateforme_</h1>
+    <section class="d-flex flex-row justify-content-around">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="papillon.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Un papillon</h5>
+                <p class="card-text">Un papillon, c'est un peu comme une chenille, mais avec des ailes, Ne pas ingerer.</p>
+                <a href="#" class="btn btn-primary">Commandez votre propre papillon</a>
+            </div>
+        </div>
+<article class='alert alert-secondary rounded w-50 p-3 .bg-light'>
+<h1>Bonjour, monde!</h1>
+<p>Il existe plusieurs vison du terme:</p>
+<p>le monde est la matiere , l'espace et les phénomènes qui nous sont accessibles par les sens, l'expérience ou la raison </p>
+<p>le sens le plus courant  désigne notre planete, la Terre, avec ses habitants, et son environnement plus ou moins naturel.</p>
+<hr/>
+<p>Le sens étendue désigne l'univers dans son ensemble.</p>
+<button type="button" class="btn btn-danger">Rebooter le monde</button>
+<div class="spinner-border text-primary" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+<nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-end">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+
+</article>
+<div class="list-group">
+
+  <button type="button" class="list-group-item list-group-item-action active">Limbes</button>
+  <button type="button" class="list-group-item list-group-item-action">Luxure</button>
+  <button type="button" class="list-group-item list-group-item-action">Gourmandise</button>
+  <button type="button" class="list-group-item list-group-item-action" >Avarice</button>
+  <button type="button" class="list-group-item list-group-item-action">Colere</button>
+  <button type="button" class="list-group-item list-group-item-action">Hérésie</button>
+  <button type="button" class="list-group-item list-group-item-action">Violence</button>
+  <button type="button" class="list-group-item list-group-item-action">Ruse</button>
+  <button type="button" class="list-group-item list-group-item-action">Tromperie</button>
+  <button type="button" class="list-group-item list-group-item-action">Trahison</button>
+  <button type="button" class="list-group-item list-group-item-action">Internet Explorer</button>
+</div>
+
+    </section>
+    <p>Installation de AI 9000</p>
+<div class="  progress m-5 ">
+  <div class="  progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+<div class="d-flex flex-row justify-content-around">
+<section class="container d-flex flex-column w-25 p-3">
+<p class="fw-bolder">Recevez votre copie gratuite d'internet 2!</p>
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">@</span>
+                                <input type="text" class="form-control" placeholder="Login" aria-label="Login" aria-describedby="basic-addon1">
+                            </div>
+
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Mot de Passe" aria-label="Mot de Passe" aria-describedby="basic-addon2">
+                                <span class="input-group-text" id="basic-addon2">@example.com</span>
+                            </div>
+
+                            <label for="basic-url" class="form-label">URL des Internets 2 et 2.1 Beta</label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">DogeCoin</span>
+                                <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                                <span class="input-group-text">.00</span>
+                            </div>
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon3">https://l33t.lptf/dkwb/berlusconimkt/</span>
+                                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+                            </div>
+</section>
+<section class="container d-flex flex-column justify-content-center  w-25 p-3">
+                            
+                            <label>Email adress</label>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Login" aria-label="Login" aria-describedby="basic-addon1">
+                            </div>
+                            <p class="fw-light">We'll never share your email with anyone else</p>
+                                <label>Password</label>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Mot de Passe" aria-label="Mot de Passe" aria-describedby="basic-addon2">
+                            </div>
+                            <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Check me out        
+                            </label>
+
+                            </div>
+
+                <a href="#" class="btn btn-primary w-25">Submit </a>
+</section>
+</div>
+
+
+    </main>
+</body>
+</html>
