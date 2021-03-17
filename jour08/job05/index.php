c019eeae1e7ebf3839ff33196c6791279aad1501
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-03-17 13:57:41 +0100

    center

--- jour08/job05/index.php
+++ jour08/job05/index.php
@@ -34,36 +34,37 @@ de différentes tailles.-->
         </nav>
     </header>
     <main>
-
-    <div class="row">
-    <div class="col s7 m4">
-      <div class="card">
-        <div class="card-image">
-          <img src="https://i.gifer.com/origin/9c/9c184bea0d1d2972e749e3f09a96e3d6.gif"  class="responsive-img" style="height:500px; width:600px;">
-          <span class="card-title">PHONK</span>
+  <section class="row container">
+  <h1>Les plus populaires</h1>
+    <!-- <div class="row"> -->
+      <div class="col s5 m5">
+        <div class="card">
+          <div class="card-image  ">
+            <img src="https://i.gifer.com/origin/9c/9c184bea0d1d2972e749e3f09a96e3d6.gif" style="height:400px; width:500px;">
+            <span class="card-title">PHONK</span>
+          </div>
+          <div class="card-content">
+            <p class='center'>
+            Simple et stréréotypé par son 808 la Phonk à pourtant beaucoup de choses a offrir à partir de ses rythmes en diablé et 
+            son style s'associant a toutes les styles de rap. On retrouve souvent cette musique associé a des clip de drift
+            </p>
+          </div>
+          <div class="card-action">
+            <a href="https://www.youtube.com/playlist?list=PL1hmYleVaXpA-aVQjX-gGzewgqSzlxNu6" target="_blank">Playlist</a>
+          </div>
         </div>
-        <div class="card-content">
-          <p>
-          Simple et stréréotypé par son 808 la Phonk à pourtant beaucoup de choses a offrir à partir de ses rythmes en diablé et 
-          son style s'associant a toutes les styles de rap 
-          </p>
-        </div>
-        <div class="card-action">
-          <a href="https://www.youtube.com/playlist?list=PL1hmYleVaXpA-aVQjX-gGzewgqSzlxNu6" target="_blank">Playlist</a>
-        </div>
-      </div>
     </div>
-  </div>
+  <!-- </div> -->
   
-  <div class="row">
-    <div class="col s7 m4">
+  <!-- <div class="row"> -->
+    <div class="col s5 m5">
       <div class="card">
         <div class="card-image">
-          <img src="https://thumbs.gfycat.com/AggressiveUnluckyHanumanmonkey-size_restricted.gif"  class="responsive-img" style="height:500px; width:600px;">
+          <img src="https://thumbs.gfycat.com/AggressiveUnluckyHanumanmonkey-size_restricted.gif"   style="height:400px; width:500px;">
           <span class="card-title">AMV TV</span>
         </div>
         <div class="card-content">
-          <p>
+          <p class="center">
           Vous avez vu des animes et vous avez la flemme de revoir certains passage ainsi que l'empreinte artistique qui vous a hyper ? 
           Suivez l'AMV TV qui melange les soundcloud rapper a vos scenes d'anime préférés! 
           </p>
@@ -73,9 +74,29 @@ de différentes tailles.-->
         </div>
       </div>
     </div>
-  </div>
-
-  <div class="row">
+  <!-- </div> -->
+  </section>
+  <section class="row container" >
+  <h1>Les mieux notés</h1>
+  <!-- <div> -->
+    <div class="col s7 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="https://thumbs.gfycat.com/AnnualNextAmazontreeboa-small.gif">
+          <span class="card-title">BLAOW</span>
+        </div>
+        <div class="card-content">
+          <p  class="center">
+            La Trap dérive beaucoup des rythmique Hardcore/Metal, souvent peu signifiante en terme de lyrics mais toujours tres ambiançante 
+          </p>
+        </div>
+        <div class="card-action">
+          <a href="https://www.youtube.com/playlist?list=PL1hmYleVaXpC4mDHDFZP0Y3jMzQlwDuES">Playlist</a>
+        </div>
+      </div>
+    </div>
+  <!-- </div> -->
+  <!-- <div> -->
     <div class="col s7 m3">
       <div class="card">
         <div class="card-image">
@@ -83,7 +104,7 @@ de différentes tailles.-->
           <span class="card-title">TRANSCENDANCE</span>
         </div>
         <div class="card-content">
-          <p>
+          <p  class="center">
           Certaines musique de part leur qualité peuvent nous transcender , nous donner la chair de poul, mais en ce qui concerne la vitesse
           et le crescendo, cette playlist devrais transcender le temps qui s'écoule ainsi que la réponse synaptique et musculaire 
           </p>
@@ -93,26 +114,8 @@ de différentes tailles.-->
         </div>
       </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="col s7 m3">
-      <div class="card">
-        <div class="card-image">
-          <img src="https://thumbs.gfycat.com/AnnualNextAmazontreeboa-small.gif">
-          <span class="card-title">BLAOW</span>
-        </div>
-        <div class="card-content">
-          <p>
-            La Trap dérive beaucoup des rythmique Hardcore/Metal, souvent peu signifiante en terme de lyrics mais toujours tres ambiançante 
-          </p>
-        </div>
-        <div class="card-action">
-          <a href="https://www.youtube.com/playlist?list=PL1hmYleVaXpC4mDHDFZP0Y3jMzQlwDuES">Playlist</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
+  <!-- </div> -->
+  <!-- <div> -->
     <div class="col s7 m3">
       <div class="card">
         <div class="card-image">
@@ -120,7 +123,7 @@ de différentes tailles.-->
           <span class="card-title">ELECTRO</span>
         </div>
         <div class="card-content">
-          <p>
+          <p  class="center">
           Les français sont les meilleurs en electro, découvrez une playlist émouvatante par les son electro recherchés et travaillés
           </p>
         </div>
@@ -129,7 +132,8 @@ de différentes tailles.-->
         </div>
       </div>
     </div>
-  </div>
+  <!-- </div> -->
+  </section>
     </footer>
 </body>
 </html>
